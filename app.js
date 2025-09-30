b44bde564b1a46e721ff8e9fc913d98bd23db242
adrbutz-byte <adrbutz@gmail.com>
2025-09-30 20:34:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-// Script para o botão
+// Script para o botão de alerta
 document.getElementById('btnClique').addEventListener('click', function() {
     alert('Você clicou no botão!');
 });
+
+// Código do agendamento assumindo que o HTML tem o código do index atualizado sem JS embutido
+// (Recomendo separar o JS do agendamento em outro arquivo para organização, mas pode manter junto)
